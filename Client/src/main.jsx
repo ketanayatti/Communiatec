b8890c090414d3aa228315b09eec25639a703bb8
Kethan Ayatti <kethanayatti333@gmail.com>
2025-11-29 20:25:19 +0530

    Fix: Refactor import statement in main.jsx to include useLayoutEffect for improved functionality

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { registerSW } from "virtual:pwa-register";
 import App from "./App.jsx";
